42c4a55de021f3e393ad9e6c144ecb8923dcc22d
Jitendranath Tudu <jitendratd@gmail.com>
2020-09-29 14:47:27 +0530

    Revert "Error in the sign up resolved"
    This reverts commit cc92ea7b52188f33c0e8876bf03c3e8d1956c058.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SignUp from './SignUp';
-// import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
